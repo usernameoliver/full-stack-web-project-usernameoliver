55a464100841f8ff3c3e274b3b15fd30c19016fb
usernameoliver <Oliverhaobit@gmail.com>
2016-11-06 11:55:59 -0500

    added dependency in procfile in windows format and modified main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,11 +12,29 @@ import spark.ModelAndView;
 import static spark.Spark.get;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 public class Main {
 
-    public static void main(String[] args) {
-    System.out.println("hello");
+  public static void main(String[] args) {
+
+    port(Integer.valueOf(System.getenv("PORT")));
+    staticFileLocation("/public");
+
+    get("/hello", (req, res) -> "Hello World");
+
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
+
+
   }
+  
+  
 
 }
