d3726232e980c587ce763754a411376e03e0378c
usernameoliver <Oliverhaobit@gmail.com>
2016-11-06 12:17:12 -0500

    modified main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,15 +23,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
-
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
-
+    //get("/hello", (req, res) -> "Hello World");
 
   }
   
