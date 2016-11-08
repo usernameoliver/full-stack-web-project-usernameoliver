07a38f81f1515d9c1731909e4fbb03cf33b05c2b
OliverHao <olvierhaobit@gmail.com>
2016-11-08 15:45:29 -0500

    added user.html

---
+++ src/main/resources/public/js/my.js
@@ -0,0 +1,10 @@
+fs.stat('foo.txt', function(err, stat) {
+    if(err == null) {
+        console.log('File exists');
+    } else if(err.code == 'ENOENT') {
+        // file does not exist
+        fs.writeFile('log.txt', 'Some log\n');
+    } else {
+        console.log('Some other error: ', err.code);
+    }
+});
