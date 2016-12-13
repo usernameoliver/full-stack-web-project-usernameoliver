8fd9b7db2f4609208766528034af1dd5f9c16a85
OliverHao <olvierhaobit@gmail.com>
2016-12-13 11:05:47 -0500

    prevent ; stops travis

--- src/main/resources/public/js/my.js
+++ src/main/resources/public/js/my.js
@@ -49,7 +49,7 @@ window.onclick = function(event) {
       }
     }
   }
-}
+};
 
 
 // Get the modal
@@ -64,16 +64,16 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
