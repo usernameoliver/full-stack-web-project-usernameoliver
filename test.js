b5ffce8c83d306b3bcca86a79ed31da6c3aede67
OliverSurfaceBook <oliverhaobit@gmail.com>
2017-02-08 15:32:44 -0500

    removed trash test files

--- test.js
+++
@@ -1,5 +0,0 @@
-var exec = require('child_process').exec;
-
-exec('java -Xmx10g -cp target/classes:target/dependency/* edu.washington.nsre.extraction.NewsSpikePredict https://s3-us-west-2.amazonaws.com/eventextractionmodelbucket/data/scale/model https://s3-us-west-2.amazonaws.com/eventextractionmodelbucket/data/test  ./predict', function(error, stdout, stderr) {
-    console.log(stdout);
-});
